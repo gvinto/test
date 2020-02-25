6f81e5a55440b3fbb3c69346da858d5ecbb5b0ab
gvinto <gvinto@gmail.com>
2020-02-25 13:40:58 +0800

    Updates

---
+++ precache-manifest.f7e821eb33f785703a3e0e51cc71c524.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8507bc7247df0d08de1874ef82a31c5",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2f904d90507f2662015c",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fef3b8016227f40ce248",
+    "url": "/test/static/js/2.077fa91f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test/static/js/2.077fa91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f904d90507f2662015c",
+    "url": "/test/static/js/main.97f47d41.chunk.js"
+  },
+  {
+    "revision": "21648e4d2a7621fce758",
+    "url": "/test/static/js/runtime-main.ee2a3769.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
